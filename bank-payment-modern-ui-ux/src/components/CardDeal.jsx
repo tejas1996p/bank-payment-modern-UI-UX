fd8a1a3edcd601b6e1ade7571d719c78740d7c6d
Tejaskumar Sureshbhai Patel <tejas1996p@gmail.com>
2023-03-26 15:46:13 +0100

    init

---
+++ bank-payment-modern-ui-ux/src/components/CardDeal.jsx
@@ -0,0 +1,24 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      To find a better card deal for your bank payment website, start by analyzing your current payment processing fees and comparing them to industry benchmarks. Consider negotiating with payment processors to obtain better rates or explore other payment solutions that offer lower fees and improved features.</p>
+
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
